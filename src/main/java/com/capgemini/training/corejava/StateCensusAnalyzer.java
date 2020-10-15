f4642bbe32557428b727b84eb546c8667657d228
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-16 02:04:05 +0530

    implement: check for correct delimiter

--- src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
+++ src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
@@ -1,12 +1,12 @@
 package com.capgemini.training.corejava;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import com.opencsv.CSVReader;
-
 import org.apache.commons.io.FilenameUtils;
 
 /**
@@ -20,17 +20,39 @@ public final class StateCensusAnalyzer {
     }
 
     public void load(String filePath) throws StateCensusAnalyzerException {
+        final String[] expectedHeader = { "Sr No.", "State Name", "TIN", "Population", "State Code" };
         if (!FilenameUtils.getExtension(filePath).equalsIgnoreCase("CSV"))
             throw new StateCensusAnalyzerException("Wrong file extension!");
 
-            try (Reader reader = Files.newBufferedReader(Paths.get(filePath));
-                    CSVReader csvReader = new CSVReader(reader);) {
-                this.dataPath = filePath;
-            } catch (IOException e) {
-                throw new StateCensusAnalyzerException();
-            } catch (NullPointerException e2) {
-                throw new StateCensusAnalyzerException("File does not exists");
-            }
+        try (Reader reader = Files.newBufferedReader(Paths.get(filePath));
+                CSVReader csvReader = new CSVReader(reader);) {
+            if (!checkDelimiter(filePath, ','))
+                throw new StateCensusAnalyzerException("Illegal separator!");
+            if (!checkHeader(csvReader,expectedHeader))
+                throw new StateCensusAnalyzerException("Wrong column header!");
+
+            this.dataPath = filePath;
+        } catch (IOException e) {
+            throw new StateCensusAnalyzerException();
+        } catch (NullPointerException e2) {
+            throw new StateCensusAnalyzerException("File does not exists");
+        }
+    }
+
+    private boolean checkHeader(CSVReader csvReader,String[] expectedHeader) throws IOException {
+        
+        String[] header = csvReader.peek();
+        for (int i = 0; i < header.length; i++) {
+            if(!header[i].equalsIgnoreCase(expectedHeader[i]))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean checkDelimiter(String filepath, char c) throws IOException {
+        BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filepath));
+        String[] columns = bufferedReader.readLine().split(String.valueOf(c));
+        return columns.length == 5;
     }
 
     public int countEntries() throws StateCensusAnalyzerException {
