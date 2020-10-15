25aeddd4d52cb7b721d26c3bca53a52c9ef4543f
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-16 02:33:16 +0530

    refactor: used final

--- src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
+++ src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
@@ -12,11 +12,11 @@ import org.apache.commons.io.FilenameUtils;
 public final class StateCensusAnalyzer {
     private String dataPath;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Welcome to India State Census Analyzer!");
     }
 
-    public void load(String filePath) throws StateCensusAnalyzerException {
+    public void load(final String filePath) throws StateCensusAnalyzerException {
         final String[] expectedHeader = { "Sr No.", "State Name", "TIN", "Population", "State Code" };
         if (!FilenameUtils.getExtension(filePath).equalsIgnoreCase("CSV"))
             throw new StateCensusAnalyzerException("Wrong file extension!");
@@ -29,16 +29,16 @@ public final class StateCensusAnalyzer {
                 throw new StateCensusAnalyzerException("Wrong column header!");
 
             this.dataPath = filePath;
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new StateCensusAnalyzerException();
-        } catch (NullPointerException e2) {
+        } catch (final NullPointerException e2) {
             throw new StateCensusAnalyzerException("File does not exists");
         }
     }
 
-    private static boolean checkHeader(CSVReader csvReader, String[] expectedHeader) throws IOException {
+    private static boolean checkHeader(final CSVReader csvReader, final String[] expectedHeader) throws IOException {
 
-        String[] header = csvReader.peek();
+        final String[] header = csvReader.peek();
         for (int i = 0; i < header.length; i++) {
             if (!header[i].equalsIgnoreCase(expectedHeader[i]))
                 return false;
@@ -46,9 +46,9 @@ public final class StateCensusAnalyzer {
         return true;
     }
 
-    private static boolean checkDelimiter(String filepath, char c) throws IOException {
-        BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filepath));
-        String[] columns = bufferedReader.readLine().split(String.valueOf(c));
+    private static boolean checkDelimiter(final String filepath, final char c) throws IOException {
+        final BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filepath));
+        final String[] columns = bufferedReader.readLine().split(String.valueOf(c));
         return columns.length == 5;
     }
 
@@ -60,7 +60,7 @@ public final class StateCensusAnalyzer {
             while (csvReader.readNext() != null)
                 count++;
 
-        } catch (Exception e) {
+        } catch (final Exception e) {
             throw new StateCensusAnalyzerException();
         }
         return count;
