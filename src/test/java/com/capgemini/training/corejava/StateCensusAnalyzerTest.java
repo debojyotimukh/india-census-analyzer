caf69decccedf8ae6d6cff1e83917316b80822a6
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-15 21:41:39 +0530

    test dataset added

---
+++ src/test/java/com/capgemini/training/corejava/StateCensusAnalyzerTest.java
@@ -0,0 +1,13 @@
+package com.capgemini.training.corejava;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class StateCensusAnalyzerTest {
+
+    @Test
+    public void demoTest() {
+        Assert.assertEquals(1, 1);
+    }
+}
