32d93ad23f661802981af1e29329f64e6a3849bd
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-15 21:57:14 +0530

    implement: csv file load and count entries

--- src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
+++ src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
@@ -1,16 +1,41 @@
 package com.capgemini.training.corejava;
 
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.opencsv.CSVReader;
+
 /**
  * Hello world!
  */
 public final class StateCensusAnalyzer {
+    private String dataPath;
 
-    /**
-     * Says hello to the world.
-     * 
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to India State Census Analyzer!");
+    }
+
+    public void load(String filePath) {
+        try (Reader reader = Files.newBufferedReader(Paths.get(filePath));
+                CSVReader csvReader = new CSVReader(reader);) {
+            this.dataPath = filePath;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int countEntries() {
+        int count = 0;
+        try (Reader reader = Files.newBufferedReader(Paths.get(dataPath));
+                CSVReader csvReader = new CSVReader(reader);) {
+
+            while (csvReader.readNext() != null)
+                count++;
+
+        } catch (Exception e) {
+            // duck
+        }
+        return count;
     }
 }
