e35da1a08961082b3614fa74698059704c0041bd
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-15 21:56:05 +0530

    Test case add: count the number of entries

--- src/test/java/com/capgemini/training/corejava/StateCensusAnalyzerTest.java
+++ src/test/java/com/capgemini/training/corejava/StateCensusAnalyzerTest.java
@@ -3,11 +3,20 @@ package com.capgemini.training.corejava;
 import org.junit.Test;
 
 import org.junit.Assert;
+import org.junit.Before;
 
 public class StateCensusAnalyzerTest {
+    public static final String FILE_PATH = "src/test/resources/IndianStateCensusData.csv";
+    public StateCensusAnalyzer censusAnalyzer;
+
+    @Before
+    public void init() {
+        censusAnalyzer = new StateCensusAnalyzer();
+    }
 
     @Test
-    public void demoTest() {
-        Assert.assertEquals(1, 1);
+    public void countEntriesCheck() {
+        censusAnalyzer.load(FILE_PATH);
+        Assert.assertEquals(37, censusAnalyzer.countEntries());
     }
 }
