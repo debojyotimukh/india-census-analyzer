dc9fb8c750c587a8726ed174bb8857f8dcc1126d
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-15 21:42:13 +0530

    StateCensusAnalyzer class created

---
+++ src/main/java/com/capgemini/training/corejava/StateCensusAnalyzer.java
@@ -0,0 +1,16 @@
+package com.capgemini.training.corejava;
+
+/**
+ * Hello world!
+ */
+public final class StateCensusAnalyzer {
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
