3f152f419406dbaa9da9ea48bd4514a51da4c997
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-18 02:09:51 +0530

    formatting

--- src/main/java/com/capgemini/training/corejava/StateCensusAnalyzerException.java
+++ src/main/java/com/capgemini/training/corejava/StateCensusAnalyzerException.java
@@ -7,11 +7,18 @@ public class StateCensusAnalyzerException extends Exception{
      */
     private static final long serialVersionUID = -9183023909529475627L;
 
+     
+
     public StateCensusAnalyzerException() {
+        super();
     }
 
     public StateCensusAnalyzerException(String message) {
         super(message);
+    }
+
+    public StateCensusAnalyzerException(String message, Throwable cause) {
+        super(message, cause);
     }
 
 }
