a71dd3d36c3ba9c19ad6125a8696ad5f1f3517d6
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-18 05:31:47 +0530

    documentation

--- src/main/java/com/capgemini/training/corejava/CSVBuilderFactory.java
+++ src/main/java/com/capgemini/training/corejava/CSVBuilderFactory.java
@@ -1,6 +1,11 @@
 package com.capgemini.training.corejava;
 
 public class CSVBuilderFactory<T> {
+    /**
+     * Factory method to create CSV builder
+     * @param <T>
+     * @return generic CSV builder
+     */
     public static <T> ICSVBuilder<T> createCSVBuilder() {
         return new OpenCSVBuilder<>();
     }
--- src/main/java/com/capgemini/training/corejava/OpenCSVBuilder.java
+++ src/main/java/com/capgemini/training/corejava/OpenCSVBuilder.java
@@ -2,6 +2,7 @@ package com.capgemini.training.corejava;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -13,6 +14,7 @@ public class OpenCSVBuilder<T> implements ICSVBuilder<T> {
 
     @Override
     public Iterable<T> getCSVFileIterable(Reader reader, Class<T> clazz) throws CSVBuilderException {
+
         try {
             CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader).withSeparator(',').withThrowExceptions(true)
                     .withType(clazz).build();
@@ -24,6 +26,19 @@ public class OpenCSVBuilder<T> implements ICSVBuilder<T> {
             throw new CSVBuilderException(re.getMessage());
         }
 
+    }
+
+    @Override
+    public List<T> getLst(Reader reader, Class<T> clazz) throws CSVBuilderException {
+
+        try {
+            CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader).withSeparator(',').withThrowExceptions(true)
+                    .withType(clazz).build();
+            return csvToBean.parse();
+
+        } catch (RuntimeException re) {
+            throw new CSVBuilderException(re.getMessage());
+        }
     }
 
 }
